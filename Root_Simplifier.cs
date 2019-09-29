394bd8938cb22e30d6df34dd724463e024d6c1c6
Relaxo143 <Relaxo143@protonmail.com>
2019-09-29 17:57:25 +0300

    Improve UI

--- Root_Simplifier.cs
+++ Root_Simplifier.cs
@@ -8,30 +8,30 @@ namespace Root_Simplifier
         static void Main()
         {
         A:
-            int exponent, multi = 0 ;
-            double value; 
+            int exponent, multi = 0;
+            double value;
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Root Simplifier 1.0");
-            Console.WriteLine("Enter exponetial number!");
+            Console.WriteLine("Specify the radical exponent:");
             try
             {
                 exponent = int.Parse(Console.ReadLine());
             }
-            catch(Exception)
+            catch (Exception)
             {
-                Console.WriteLine("Enter only numbers , please!");
+                Console.WriteLine("Enter only numbers, please!");
                 Thread.Sleep(3000);
                 Console.Clear();
                 goto A;
             }
-            if (exponent <= 0)
+            if (exponent <= 1)
             {
                 Console.WriteLine("Enter only positive numbers, please!");
                 Thread.Sleep(3000);
                 Console.Clear();
                 goto A;
             }
-            Console.WriteLine("Enter the value under the root!");
+            Console.WriteLine("Specify a value under the radical sign!");
             try
             {
                 value = int.Parse(Console.ReadLine());
@@ -44,18 +44,31 @@ namespace Root_Simplifier
                 goto A;
             }
 
-                for (int i = 1; i <= value; i++)
+            for (int i = 1; i <= value; i++)
+            {
+                if (value % Math.Pow(i, exponent) == 0)
                 {
-                    if (value % Math.Pow(i,exponent) == 0)
-                    {
-                        multi = i;
-                    }
-
+                    multi = i;
                 }
 
-            value = value / Math.Pow(multi, exponent);
-            Console.WriteLine("number = " + multi + "  " + exponent + " rt " + "" + value);
+            }
 
+            value = value / Math.Pow(multi, exponent);
+
+            if (exponent == 2)
+            {
+                Console.WriteLine("Simplified expression = " + multi + " x " + exponent + "nd root of (" + "" + value + ")");
+            }
+            else if (exponent == 3)
+            {
+                Console.WriteLine("Simplified expression = " + multi + " x " + exponent + "rd root of (" + "" + value + ")");
+            }
+            else
+            {
+                Console.WriteLine("Simplified expression = " + multi + " x " + exponent + "th root of (" + "" + value + ")");
+            }
+
+            Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
             Console.Clear();
             goto A;
